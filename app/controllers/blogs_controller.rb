fe2f4f8589b56d2293e5726b0bb2e15ff24284f4
yasmaimo <yasmaimo0141@gmail.com>
2018-05-09 17:30:22 +0900

    Finish 4-4

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,8 +1,10 @@
 class BlogsController < ApplicationController
   def index
+  	  @blogs = Blog.all
   end
 
   def show
+  	  @blog = Blog.find(params[:id])
   end
 
   def new
@@ -10,6 +12,9 @@ class BlogsController < ApplicationController
   end
 
   def create
+      blog = Blog.new(blog_params)
+      blog.save
+      redirect_to blogs_path
   end
 
   def edit
