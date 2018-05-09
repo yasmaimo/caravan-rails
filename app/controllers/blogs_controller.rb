ef054e4d42a8d5b98705f0976c13168c9abd559f
yasmaimo <yasmaimo0141@gmail.com>
2018-05-09 17:08:53 +0900

    Finish 4-3

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,8 +6,19 @@ class BlogsController < ApplicationController
   end
 
   def new
+  	  @blog = Blog.new
+  end
+
+  def create
   end
 
   def edit
   end
+
+  private
+
+  def blog_params
+  	  params.require(:blog).permit(:title, :category, :body)
+  end
+
 end
